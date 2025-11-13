cd5341995996207c2572b7da6c1b666b93a9681c
iamdavef3-sketch <iamdave.f3@gmail.com>
2025-11-12 19:28:53 -0500

    Update sendUpdate.js

--- sendUpdate.js
+++ sendUpdate.js
@@ -16,7 +16,7 @@ async function sendUpdate() {
   if (days > 0) {
     message = `# WALPURGIS NIGHT UPDATE\nManager it is ${days} days until **Walpurgisnacht**!`;
   } else if (days === 0) {
-    message = `# Today is WALPURGISNACHT!\n@Walpurgisnacht `;
+    message = `# Today is WALPURGISNACHT!\n@Walpurgisnacht`;
   } else {
     message = `# WALPURGIS NIGHT UPDATE\nThe ritual has passed. Await for the next Walpurgisnacht.`;
   }
