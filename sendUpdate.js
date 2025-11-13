ee13496122d84bd116574cf3d4a2b4323076034d
iamdavef3-sketch <iamdave.f3@gmail.com>
2025-11-13 06:53:45 -0500

    Update sendUpdate.js

--- sendUpdate.js
+++ sendUpdate.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import dayjs from "dayjs";
-import timezone from "dayjs/plugin/timezone"; // Time zone support
+import timezone from "dayjs/plugin/timezone.js";
 import utc from "dayjs/plugin/utc"; // UTC plugin for consistency
 
 // Extend dayjs with timezone and UTC plugins
