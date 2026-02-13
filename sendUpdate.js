1fef9721b84faac2d1cc08fbaf147bc956f1a46a
Randomz <iamdave.f3@gmail.com>
2026-02-13 15:14:35 -0500

    Update sendUpdate.js

--- sendUpdate.js
+++ sendUpdate.js
@@ -16,7 +16,7 @@ const FLORIDA_TIMEZONE = "America/New_York";
 const CUSTOM_FOOTER = process.env.CUSTOM_FOOTER || "";
 
 // --- CONFIGURATION ---
-const WALPURGIS_DATE = "2026-02-13";
+const WALPURGIS_DATE = "2026-02-18";
 const WALPURGIS_PING_HOUR = 17; // 17:00 Military Time = 5:00 PM
 
 // Date Asher became bald (Set to today: Jan 7, 2026)
